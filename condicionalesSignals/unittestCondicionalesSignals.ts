e93b493e1c8348fbb51a3d25f5ec995ef171f777
Franki Briones <61076196+franki-wolf1@users.noreply.github.com>
2024-10-04 08:58:17 -0500

    Update unittestCondicionalesSignals.ts

--- condicionalesSignals/unittestCondicionalesSignals.ts
+++ condicionalesSignals/unittestCondicionalesSignals.ts
@@ -1,1 +1,22 @@
 
+// conditional.service.spec.ts
+import { TestBed } from '@angular/core/testing';
+import { ConditionalService } from './conditional.service';
+
+describe('ConditionalService', () => {
+  let service: ConditionalService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ConditionalService);
+  });
+
+  it('should follow the correct path based on signal value', () => {
+    service.evaluateCondition();
+    expect(service.getResultSignal()()).toEqual('PathA');
+
+    service.getConditionSignal().set(false);
+    service.evaluateCondition();
+    expect(service.getResultSignal()()).toEqual('PathB');
+  });
+});
